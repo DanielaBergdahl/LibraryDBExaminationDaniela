88729e14a37ec73c4a35d5783ae86a07d3f130f6
DanielaBergdahl <dcode@rdqt.se>
2022-01-14 10:07:37 +0100

    La till Loan .Funkar

---
+++ Data/Entities/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryDBExaminationDaniela.Data.Entities
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public DateTime DateBorrowed { get; set; }
+        public DateTime DateReturned { get; set; }
+        public int LibraryBookId { get; set; }
+        public int BorrowerId { get; set; }
+        public LibraryBook LibraryBook { get; set; }
+        public Borrower Borrower { get; set; }
+    }
+}
